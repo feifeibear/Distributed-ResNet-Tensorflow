a1b0b154f1260ee786413aba5a45e494a64d5b0d
feifeibear <fangjiarui123@gmail.com>
2018-02-09 16:52:20 -0800

    test horovod on muradin

--- mac_scripts/submit_imagenet_horovod_train_mac.sh
+++ mac_scripts/submit_imagenet_horovod_train_mac.sh
@@ -1,4 +1,5 @@
-export TF_SCRIPT="../resnet_imagenet_main.py"
+. /home/fangjr/Envs/horovod/bin/activate
+export TF_SCRIPT="../resnet_imagenet_main_horovod.py"
 export DATASET=imagenet
 export TF_FLAGS="
   --train_data_path=.. \
@@ -6,12 +7,14 @@ export TF_FLAGS="
   --train_dir=./tmp/resnet_model/train \
   --dataset=${DATASET} \
   --mode=train \
-  --num_gpus=1 \
-  --batch_size=1 \
+  --num_gpus=0 \
+  --batch_size=128 \
   --sync_replicas=True \
   --train_steps=112600 \
   --num_epochs=90 \
   --benchmark_mode=True
 "
 
-python3 $TF_SCRIPT $TF_FLAGS
+mpirun -np 4 python3 $TF_SCRIPT $TF_FLAGS
+
+deactivate
--- mac_scripts/submit_imagenet_mac_single.sh
+++ mac_scripts/submit_imagenet_mac_single.sh
@@ -1,4 +1,3 @@
-. /home/fangjr/Envs/horovod/bin/activate
 export TF_SCRIPT="../resnet_imagenet_main.py"
 export DATASET=imagenet
 export TF_FLAGS="
@@ -7,14 +6,12 @@ export TF_FLAGS="
   --train_dir=./tmp/resnet_model/train \
   --dataset=${DATASET} \
   --mode=train \
-  --num_gpus=0 \
-  --batch_size=1 \
+  --num_gpus=1 \
+  --batch_size=128 \
   --sync_replicas=True \
   --train_steps=112600 \
   --num_epochs=90 \
   --benchmark_mode=True
 "
 
-mpirun -np 4 python3 $TF_SCRIPT $TF_FLAGS
-
-deactivate
+python3 $TF_SCRIPT $TF_FLAGS
